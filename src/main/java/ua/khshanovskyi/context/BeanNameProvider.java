c8ca1b693a859f868e0052e10aa210c827de1381
Pavlo Khshanovskyi <pkhshan@emory.edu>
2022-07-04 13:13:10 +0300

    Changes

--- src/main/java/ua/khshanovskyi/context/BeanNameProvider.java
+++ src/main/java/ua/khshanovskyi/context/BeanNameProvider.java
@@ -6,14 +6,14 @@ public class BeanNameProvider {
     private BeanNameProvider() {
     }
 
-    static <T> String provideNameForBean(Class<T> beanType) {
+    static String provideNameForBean(Class<?> beanType) {
         String beanName = beanType.getAnnotation(Bean.class).beanName();
         return beanName.equals("") ?
                 nameToLoverCaseFirstLetter(beanType) :
                 beanName;
     }
 
-    private static <T> String nameToLoverCaseFirstLetter(Class<T> beanType) {
+    private static String nameToLoverCaseFirstLetter(Class<?> beanType) {
         String name = beanType.getSimpleName();
         return name.replace(name.charAt(0), Character.toLowerCase(name.charAt(0)));
     }
